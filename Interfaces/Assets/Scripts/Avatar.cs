5657f7e337a8a2fef285c4513d980b60bbf67706
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 21:41:31 +0100

    Adding project.

---
+++ Interfaces/Assets/Scripts/Avatar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Avatar : MonoBehaviour, IKillable, IDamageable<float>
+{
+    /// <inheritdoc cref="IKillable.Kill"/>
+    public void Kill()
+    {
+        // Do something fun.
+    }
+
+    /// <inheritdoc cref="IDamageable{T}.Damage"/>
+    public void Damage(float damageTaken)
+    {
+        // Do something fun.
+    }
+}
---
+++ Interfaces/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// This is a basic interface with a single required method.
+/// </summary>
+public interface IKillable
+{
+    /// <summary>
+    /// Kill logic.
+    /// </summary>
+    void Kill();
+}
+    
+/// <summary>
+/// This is a generic interface where T is a placeholder for a data type that will be provided by the  implementing
+/// class.
+/// </summary>
+/// <typeparam name="T">Any data type.</typeparam>
+public interface IDamageable<in T>
+{
+    /// <summary>
+    /// Damage logic.
+    /// </summary>
+    /// <param name="damageTaken">Any data type.</param>
+    void Damage(T damageTaken);
+}
